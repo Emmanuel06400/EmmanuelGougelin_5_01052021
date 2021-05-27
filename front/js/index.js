eb2b882c7a9d2b495b1c74c1260610b9a6a57b7e
Emmanuel Gougelin <manugougelin@gmail.com>
2021-05-27 14:07:13 +0200

    elements créé avec html

--- front/js/index.js
+++ front/js/index.js
@@ -1,17 +1,18 @@
-fetch("http://localhost:3000/api/teddies") //Pointer les données de l'API du serveur
+fetch("http://localhost:3000/api/teddies") //Je pointe les données de l'API du serveur avec la méthode fetch
     .then(function(response) { //fonction pour nous retourné les données
-        var myJSON_promise = response.json();  //transformer les données en format json
-        myJSON_promise.then(function(myJSON) {
-            for (let i = 0; i < 5; i++) { //Affichage des données selon le teddy choisi : de 0 à 4
-                let nameTeddies = document.createTextNode(myJSON[i].name); //variable pour pointer et afficher la donnée 
-                let descriptionTeddies = document.createTextNode(myJSON[i].description);
-                let priceTeddies = document.createTextNode(myJSON[i].price + " euros");
-                let imageTeddies = myJSON[i].imageUrl;
-                document.getElementById("nom_teddy" + i).appendChild(nameTeddies); // A partir d'une id HTML, enfanter la variable creer
-                document.getElementById("description_teddy" + i).appendChild(descriptionTeddies);
-                document.getElementById("prix_teddy" + i).appendChild(priceTeddies);
-                document.getElementById("image_teddy" + i).setAttribute("src", imageTeddies);
-            }
-        });
+        var myJSON_promise = response.json();  //Je transforme les données en format json
+        return myJSON_promise
     }) 
+    .then(function(myJSON) {
+        for (let i = 0; i < myJSON.length; i++) { //Avec une boucle fo j'affiche les données selon le teddy choisi : de 0 à 4
+            let nameTeddies = document.createTextNode(myJSON[i].name); //je créé une variable pour afficher les données 
+            let descriptionTeddies = document.createTextNode(myJSON[i].description);//idem
+            let priceTeddies = document.createTextNode(myJSON[i].price + " euros");//idem
+            let imageTeddies = myJSON[i].imageUrl;//idem
+            document.getElementById("nom_teddy" + i).appendChild(nameTeddies); // A partir d'une id HTML, j'enfante la variable créé précedemment
+            document.getElementById("description_teddy" + i).appendChild(descriptionTeddies);
+            document.getElementById("prix_teddy" + i).appendChild(priceTeddies);
+            document.getElementById("image_teddy" + i).setAttribute("src", imageTeddies);// A partir d'une id HTML, je rajoute une attribut pour l'image 
+        }
+    });
 
--- front/js/produit.js
+++ front/js/produit.js
@@ -13,31 +13,33 @@ function $_GET(param) {// Fonction pour récupérer les paramètre dans l'URL
 	return vars;
 }
 
-fetch("http://localhost:3000/api/teddies/" + $_GET('id')) //Permet d'afficher les informations du teddy selon l'id de l'URL
-    .then(function(response) {
-        let myJSON_promise = response.json();
-        myJSON_promise.then(function(myJSON) {
-            let price_teddy = document.createTextNode(myJSON.price + " euros");
-            let name_teddy = document.createTextNode(myJSON.name);
-            let image_teddy = myJSON.imageUrl;
-            let description_teddy = document.createTextNode(myJSON.description)
-            document.getElementById("nom_produit").appendChild(name_teddy);
-            document.getElementById("prix_produit").appendChild(price_teddy);
-            document.getElementById("image_produit").setAttribute("src", image_teddy);
-            document.getElementById("desc_produit").appendChild(description_teddy);
+fetch("http://localhost:3000/api/teddies/" + $_GET('id')) // J'affiche les informations du teddy selon la key id de l'URL
+    .then(function(response) { //fonction pour nous retourné les données
+        let myJSON_promise = response.json();//Je transforme les données en format json
+        return myJSON_promise
+    })
+    .then(function(myJSON) {
+        let price_teddy = document.createTextNode(myJSON.price + " euros");//je créé une variable pour afficher les données
+        let name_teddy = document.createTextNode(myJSON.name);
+        let image_teddy = myJSON.imageUrl;
+        let description_teddy = document.createTextNode(myJSON.description)
+        document.getElementById("nom_produit").appendChild(name_teddy);// A partir d'une id HTML, j'enfante la variable créé précedemment
+        document.getElementById("prix_produit").appendChild(price_teddy);
+        document.getElementById("image_produit").setAttribute("src", image_teddy);
+        document.getElementById("desc_produit").appendChild(description_teddy);
 
-            let nb_colors = myJSON.colors;
-            for (let j = 0; j < nb_colors.length; j++) { //Affiche le nombre de couleurs du teddy selon l'API
-                let newOption = document.createElement("option");
-                let afficherCouleur = document.createTextNode(myJSON.colors[j]);
-                document.getElementById("couleur").appendChild(newOption).setAttribute("id", "color" + j);
-                document.getElementById("color" + j).setAttribute("value", j)
-                document.getElementById("color" + j).appendChild(afficherCouleur);
-            }
+        let nb_colors = myJSON.colors;
+        for (let j = 0; j < nb_colors.length; j++) { //Avec une boucle, je repète l'opération en fonction du nombre de couleurs du teddy 
+            let newOption = document.createElement("option");//je créé une variable qui génère l'élément option
+            let afficherCouleur = document.createTextNode(myJSON.colors[j]);//je créé une variable pour afficher les couleurs
+            document.getElementById("couleur").appendChild(newOption).setAttribute("id", "color" + j);// A partir d'une id HTML, j'enfante la variable créé précedemment
+            document.getElementById("color" + j).setAttribute("value", j)// A partir d'une id HTML, je rajoute une attribut
+            document.getElementById("color" + j).appendChild(afficherCouleur);
+        }
 
 
-        })
     })
  
+
 
 
