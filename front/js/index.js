8013a6dac345c3db57429adde0bd6d67e8b88483
Emmanuel Gougelin <manugougelin@gmail.com>
2021-05-29 08:01:01 +0200

    code à revoir en fonction

--- front/js/index.js
+++ front/js/index.js
@@ -2,8 +2,10 @@ fetch("http://localhost:3000/api/teddies") //Je pointe les données de l'API du 
     .then(function(response) { //fonction pour nous retourné les données
         var myJSON_promise = response.json();  //Je transforme les données en format json
         return myJSON_promise
+        
     }) 
     .then(function(myJSON) {
+
         for (let i = 0; i < myJSON.length; i++) { //Avec une boucle fo j'affiche les données selon le teddy choisi : de 0 à 4
             let nameTeddies = document.createTextNode(myJSON[i].name); //je créé une variable pour afficher les données 
             let descriptionTeddies = document.createTextNode(myJSON[i].description);//idem
@@ -13,6 +15,11 @@ fetch("http://localhost:3000/api/teddies") //Je pointe les données de l'API du 
             document.getElementById("description_teddy" + i).appendChild(descriptionTeddies);
             document.getElementById("prix_teddy" + i).appendChild(priceTeddies);
             document.getElementById("image_teddy" + i).setAttribute("src", imageTeddies);// A partir d'une id HTML, je rajoute une attribut pour l'image 
+        
+            let boxTeddie = document.createElement('div');
+            boxTeddie.classList.add('boxTeddie');
+
+            
         }
     });
 
--- front/js/produit.js
+++ front/js/produit.js
@@ -35,11 +35,57 @@ fetch("http://localhost:3000/api/teddies/" + $_GET('id')) // J'affiche les infor
             document.getElementById("couleur").appendChild(newOption).setAttribute("id", "color" + j);// A partir d'une id HTML, j'enfante la variable créé précedemment
             document.getElementById("color" + j).setAttribute("value", j)// A partir d'une id HTML, je rajoute une attribut
             document.getElementById("color" + j).appendChild(afficherCouleur);
+            if (j === 0) {
+                document.getElementById("color" + j).selected = true;
+            }
         }
 
 
     })
  
+//----------------------Le local storage---------------------//
+
+    document.getElementById("btn_ajout_panier").onclick = function ajoutArticlePanier() { //Au click la fonction d'ajout au panier s'active
+        let teddyCouleur = document.getElementById('couleur').value; //Je créé une variable pour récupérer la couleur
+        if (teddyCouleur == "defaut") { //Si pas de couleur choisie, affiche un message
+            alert("Veuillez choisir une couleur !")
+        } else {
+            //si couleur choisie, ajout du teddy dans localstorage
+            fetch("http://localhost:3000/api/teddies/" + $_GET('id'))
+            .then(function(response) {
+                let myJSON_promise = response.json();
+                return myJSON_promise
+            })
+                .then(function(myJSON) {
+    
+                    let tableau = []
+                    tableau = JSON.parse(localStorage.getItem("panier")) || []; //Avec la methode .parse je transforme une chaine json en objet jvscrit et je Créé d'un tableau avec la key panier 
+    
+                    let nouvelArticle = new Object() //Création de l'objet teddy selon les paramètres du teddy de la page
+                    nouvelArticle.name = myJSON.name;
+                    nouvelArticle.price = myJSON.price;
+                    nouvelArticle.id = myJSON._id;
+                    nouvelArticle.color = myJSON.colors[teddyCouleur];
+                    nouvelArticle.quantity = 1;
+                    nouvelArticle.img = myJSON.imageUrl;
+                    let ajoutArticle = true
+    
+                    tableau.forEach(element => { //j'incrémente la quantité du teddy si id et color sont identiques à un teddy existant
+                        if (element.id == nouvelArticle.id && element.color == nouvelArticle.color) {
+                            ajoutArticle = false;
+                            element.quantity++;
+                            localStorage.setItem("panier", JSON.stringify(tableau));//envoie les données en .json avec la methode stringify au local storage
+                        }
+                    });
+                    if (ajoutArticle) { //Si le teddy n'existe pas dans le panier, ajout du teddy dans localstorage
+                        tableau.push(nouvelArticle);
+                        localStorage.setItem("panier", JSON.stringify(tableau));
+                    }
+                })
+            }
+        }
+    
+    
 
 
 
