798ee1ef8943e17bdca6db02cbcd2d78bea27980
Emmanuel Gougelin <manugougelin@gmail.com>
2021-06-01 20:21:25 +0200

    Factorisation des produits (test)

--- front/js/index.js
+++ front/js/index.js
@@ -1,4 +1,81 @@
-fetch("http://localhost:3000/api/teddies") //Je pointe les données de l'API du serveur avec la méthode fetch
+
+    main()
+
+    async function main() {
+        getArticles()
+    }
+
+    function getArticles() {
+        return fetch("http://localhost:3000/api/teddies/")
+            .then(function(response) {
+                let myJSON_promise = response.json();
+                return myJSON_promise
+            })
+            .then(function(articles) {
+                return articles
+            })
+            .catch(function(error) {
+                alert(Error)
+            })
+            
+        }
+
+    getArticles().then(function(myJSON) {
+
+        for (let i = 0; i < myJSON.length; i++) { //Avec une boucle fo j'affiche les données selon le teddy choisi : de 0 à 4
+            let nameTeddies = document.createTextNode(myJSON[i].name); //je créé une variable pour afficher les données 
+            let descriptionTeddies = document.createTextNode(myJSON[i].description);//idem
+            let priceTeddies = document.createTextNode(myJSON[i].price + " euros");//idem
+            let imageTeddies = myJSON[i].imageUrl;//idem
+            document.getElementById("nom_teddy" + i).appendChild(nameTeddies); // A partir d'une id HTML, j'enfante la variable créé précedemment
+            document.getElementById("description_teddy" + i).appendChild(descriptionTeddies);
+            document.getElementById("prix_teddy" + i).appendChild(priceTeddies);
+            document.getElementById("image_teddy" + i).setAttribute("src", imageTeddies);// A partir d'une id HTML, je rajoute une attribut pour l'image 
+        
+
+
+                    let linkOurs = document.createElement('a');
+					linkOurs.classList.add('lienOurs');
+					linkOurs.setAttribute('href', 'HTML/produit.html?id=' + myJSON[i]._id);
+
+					let boxOurs = document.createElement('div');
+					boxOurs.classList.add('boxOurs',"card","espace","zoom");
+
+					let boxImg = document.createElement('div');
+
+					let imgOurs = document.createElement('img');
+					imgOurs.classList.add('imgOurs',"card-img-top");
+					imgOurs.setAttribute('alt', 'Ours en peluche');
+					imgOurs.setAttribute('src', myJSON[i].imageUrl);
+
+					let boxDetailOurs = document.createElement('div');
+					boxDetailOurs.classList.add('boxDetailOurs');
+
+					let nomOurs = document.createElement('h2');
+                    nomOurs.classList.add("card-title","fondBlanc",".card-body");
+					nomOurs.textContent = myJSON[i].name;
+
+					let prixOurs = document.createElement('p');
+					prixOurs.classList.add('prix',"card-text","fondBlanc",".card-body");
+					prixOurs.textContent = myJSON[i].price/100 + ' Euros';
+
+					boxImg.appendChild(imgOurs);
+
+					boxDetailOurs.appendChild(nomOurs);
+					boxDetailOurs.appendChild(prixOurs);
+
+					boxOurs.appendChild(boxImg);
+					boxOurs.appendChild(boxDetailOurs);
+
+					linkOurs.appendChild(boxOurs);
+
+					//ET ON LES AJOUTE A LA LISTE DES OURS
+					boxListeOurs.appendChild(linkOurs);
+            
+        }
+    });
+
+    /*fetch("http://localhost:3000/api/teddies") //Je pointe les données de l'API du serveur avec la méthode fetch
     .then(function(response) { //fonction pour nous retourné les données
         var myJSON_promise = response.json();  //Je transforme les données en format json
         return myJSON_promise
@@ -22,4 +99,47 @@ fetch("http://localhost:3000/api/teddies") //Je pointe les données de l'API du 
             
         }
     });
+*/
 
+
+
+
+/*
+main()
+
+    async function main() {
+        const articles = await getArticles()
+
+        for (article of articles) {
+            displayArticles()
+        }
+    }
+
+    function getArticles() {
+        return fetch("http://localhost:3000/api/teddies/")
+            .then(function(response) {
+                let myJSON_promise = response.json();
+                return myJSON_promise
+            })
+            .then(function(articles) {
+                return articles
+            })
+            .catch(function(error) {
+                alert(Error)
+            })
+            
+        }
+
+    function displayArticles(articles) {
+      document.getElementById("main").innerHTML +=
+      <article class="card espace zoom">
+              <img src="" class="card-img-top" alt="ours en peluche marron" id="image_teddy">
+              <div class="card-body">
+                <h2 class="card-title" id="nom_teddy"></h2>
+                <p class="card-text" id="description_teddy"></p>
+                <p class="card-text" id="prix_teddy"></p>
+                <a href="html/produit.html?id=5be9c8541c9d440000665243" class="btn btn-primary">Commander l'article</a>
+              </div>
+        </article>
+    }
+    */
--- front/js/produit.js
+++ front/js/produit.js
@@ -13,12 +13,15 @@ function $_GET(param) {// Fonction pour récupérer les paramètre dans l'URL
 	return vars;
 }
 
-fetch("http://localhost:3000/api/teddies/" + $_GET('id')) // J'affiche les informations du teddy selon la key id de l'URL
+function getArticlesWithId() {
+    return fetch("http://localhost:3000/api/teddies/" + $_GET('id')) // J'affiche les informations du teddy selon la key id de l'URL
     .then(function(response) { //fonction pour nous retourné les données
         let myJSON_promise = response.json();//Je transforme les données en format json
         return myJSON_promise
     })
-    .then(function(myJSON) {
+}
+
+getArticlesWithId().then(function(myJSON) {
         let price_teddy = document.createTextNode(myJSON.price + " euros");//je créé une variable pour afficher les données
         let name_teddy = document.createTextNode(myJSON.name);
         let image_teddy = myJSON.imageUrl;
@@ -36,27 +39,32 @@ fetch("http://localhost:3000/api/teddies/" + $_GET('id')) // J'affiche les infor
             document.getElementById("color" + j).setAttribute("value", j)// A partir d'une id HTML, je rajoute une attribut
             document.getElementById("color" + j).appendChild(afficherCouleur);
             if (j === 0) {
-                document.getElementById("color" + j).selected = true;
+            document.getElementById("color" + j).selected = true;
             }
         }
 
 
     })
+
  
 //----------------------Le local storage---------------------//
 
-    document.getElementById("btn_ajout_panier").onclick = function ajoutArticlePanier() { //Au click la fonction d'ajout au panier s'active
+//Fonction ajouter un produit dans le localStorage
+function ajoutProduitLocalStorage() {
+    localStorage.setItem("panier", JSON.stringify(tableau));
+  }
+
+  //Fonction tableau pour un nouvelle article selectionnée
+  function tableauNouvelArticle() {
+      
+  }
+
+    document.getElementById("btn_ajout_panier").onclick = 
+    function ajoutArticlePanier() { //Au click la fonction d'ajout au panier s'active
         let teddyCouleur = document.getElementById('couleur').value; //Je créé une variable pour récupérer la couleur
-        if (teddyCouleur == "defaut") { //Si pas de couleur choisie, affiche un message
-            alert("Veuillez choisir une couleur !")
-        } else {
-            //si couleur choisie, ajout du teddy dans localstorage
-            fetch("http://localhost:3000/api/teddies/" + $_GET('id'))
-            .then(function(response) {
-                let myJSON_promise = response.json();
-                return myJSON_promise
-            })
-                .then(function(myJSON) {
+             
+         
+        getArticlesWithId().then(function(myJSON) {
     
                     let tableau = []
                     tableau = JSON.parse(localStorage.getItem("panier")) || []; //Avec la methode .parse je transforme une chaine json en objet jvscrit et je Créé d'un tableau avec la key panier 
@@ -83,9 +91,110 @@ fetch("http://localhost:3000/api/teddies/" + $_GET('id')) // J'affiche les infor
                     }
                 })
             }
-        }
+        
+
+
+/*
+    //----------------------Nouveau test fonctionnel---------------------//
+
+            main()
+
+            function main() {
+               const articles = await getArticles()
+               const displayArticles =  displayArticles()
+               console.log(displayArticles)
+               
+           }
+       
+            function getArticles() {
+               return fetch("http://localhost:3000/api/teddies/" + $_GET('id'))
+                   .then(function(response) {
+                       let myJSON_promise = response.json();
+                       return myJSON_promise
+                   })
+                   .then(function(articles) {
+                       return articles
+                   })
+                   .catch(function(error) {
+                       alert(Error)
+                   })
+               }
+       
+               function displayArticles() {
+                   return fetch("http://localhost:3000/api/teddies/" + $_GET('id'))
+                   .then(function(myJSON) {
+                       let price_teddy = document.createTextNode(myJSON.price + " euros");//je créé une variable pour afficher les données
+                       let name_teddy = document.createTextNode(myJSON.name);
+                       let image_teddy = myJSON.imageUrl;
+                       let description_teddy = document.createTextNode(myJSON.description)
+                       document.getElementById("nom_produit").appendChild(name_teddy);// A partir d'une id HTML, j'enfante la variable créé précedemment
+                       document.getElementById("prix_produit").appendChild(price_teddy);
+                       document.getElementById("image_produit").setAttribute("src", image_teddy);
+                       document.getElementById("desc_produit").appendChild(description_teddy);
+               
+                       let nb_colors = myJSON.colors;
+                       for (let j = 0; j < nb_colors.length; j++) { //Avec une boucle, je repète l'opération en fonction du nombre de couleurs du teddy 
+                           let newOption = document.createElement("option");//je créé une variable qui génère l'élément option
+                           let afficherCouleur = document.createTextNode(myJSON.colors[j]);//je créé une variable pour afficher les couleurs
+                           document.getElementById("couleur").appendChild(newOption).setAttribute("id", "color" + j);// A partir d'une id HTML, j'enfante la variable créé précedemment
+                           document.getElementById("color" + j).setAttribute("value", j)// A partir d'une id HTML, je rajoute une attribut
+                           document.getElementById("color" + j).appendChild(afficherCouleur);
+                           if (j === 0) {
+                           document.getElementById("color" + j).selected = true;
+                           }
+                       }
+               
+               
+                   })
+               }
     
-    
+
+            function getDataPromise() {
+                return new Promise((resolve: PromiseResolve<number>, reject: PromiseReject): void => {
+                getData((error, data) => {
+                if (error) reject(error)
+                else resolve(data)
+                });
+                });
+                } 
+        
+                let ajax = function (url) {
+                    // On renvoie une promesse qui prend en paramettre une fonction
+                    // avec 2 paramètres, le callback de succès et d'erreur
+                    return new Promise(function (resolve, reject) {
+                    // Le reste du code ressemble à la méthode précédente
+                    let req = new XMLHttpRequest()
+                    req.open('GET', url, true)
+                    req.onreadystatechange = function (aEvt) {
+                    if (req.readyState == 4) {
+                    if(req.status == 200)
+                    resolve(req.responseText)
+                    else
+                    reject(req)
+                    }
+                    };
+                    req.send(null)
+                    })
+                    }
+                    
+                    // L'appel à la fonction peut se faire de cette manière là
+                    ajax('https://jsonplaceholder.typicode.com/users')
+                    .then(function (response) {
+                    // Le serveur a correctement répondu
+                    }).catch(function (req) {
+                    // Le serveur n'a pas répondu comme attendu
+                    })
 
 
 
+
+
+
+                //Fonction ajouter un produit dans le localStorage
+ const ajoutProduitLocalStorage = () => {
+    tableau.push(nouvelArticle);
+    localStorage.setItem("panier", JSON.stringify(tableau));
+  };
+
+                    */
+  
